404e57aff6e595cff5077f24ae50dfc23e390d4d
Qwetuo <chewminzhuang@hotmail.com>
2018-06-10 20:15:19 +0800

    temp update

--- script.js
+++ script.js
@@ -4,3 +4,72 @@ const tasks = [
 	'nail javascript',
 	'give feedback',
 ];
+
+// - For each todo in the tasks array, append it to the `<ul id='todo-list'></ul>`
+//   element
+
+// function appendTask(task){
+//     document.querySelector('ul').appendChild(document.createElement('li')).textContent = task;
+// }
+
+// tasks.forEach(appendTask);
+
+tasks.forEach( (task) => {
+    const newLi = document.createElement('li');
+    newLi.textContent = task;
+    document.getElementById('todo-list').appendChild(newLi);
+})
+
+// - Create a css class (e.g. `.done`) that, when applied on an element, adds the
+//   `text-decoration: line-through` property
+
+// - Add a click listener to the `<li></li>` elements that adds the css class that
+//   you created in the previous step to the element that was clicked
+
+// - Update the callback function so that clicking on the list element toggles
+//   (i.e. add/remove) the `.done` css class (hint: take a look at the
+//   documentation for `.classList.toggle()`)
+
+var linkLi = document.querySelectorAll('li');
+
+function loopEach(eachLi){
+	eachLi.addEventListener('click', (event) => {
+		event.target.classList.toggle('lineThrough')
+})
+};
+
+linkLi.forEach(loopEach);
+
+
+// ### Adding more To-Do items
+
+// - Add a `<input>` HTML element and a `<button>Add Todo</button>` HTML element.
+
+// const newLi = document.createElement('li');;
+const newInput = document.createElement('input');
+
+// document.getElementById('todo-list').appendChild(newLi).appendChild(newInput).insertAdjacentHTML('afterend', '<button>Add ToDo</button>');
+document.body.appendChild(newInput).insertAdjacentHTML('afterend', '<button>Add ToDo</button>');
+
+
+// - Add a 'click' event listener to the button that adds the value of the
+//   `<input>` element as a new todo in the `ul#todo-list` (follow the 3 steps for
+//   creating and appending a new DOM element - 
+
+
+document.querySelector('button').addEventListener('click', () => {
+// (i) `createElement`,
+	var addNewLi = document.createElement('li');
+// // (ii) change any attributes if necessary,
+	addNewLi.textContent = document.querySelector('input').value;
+// // (iii) use `appendChild` to append it to the appropriate HTML element)
+	document.getElementById('todo-list').appendChild(addNewLi);
+	document.querySelector('input').value = '';
+	});
+
+// - Add css styling and make this a nice todo list that you can show off!
+
+// - Bonus: Allow user to create a todo when they press the <kbd>Enter</kbd> key
+
+const newBtn = document.createElement('button')
+linkLi.forEach(appendChild(newBtn))
