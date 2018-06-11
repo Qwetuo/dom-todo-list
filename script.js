450f363d1ace64f3aa493d1be76ba2b19a5b4117
qwetuo <chewminzhuang@hotmail.com>
2018-06-11 10:57:33 +0800

    commit

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-const tasks = [
-	'buy milk',
-	'eat dinner',
-	'nail javascript',
-	'give feedback',
-];
+const tasks = ["buy milk", "eat dinner", "nail javascript", "give feedback"];
 
 // - For each todo in the tasks array, append it to the `<ul id='todo-list'></ul>`
 //   element
@@ -14,11 +9,11 @@ const tasks = [
 
 // tasks.forEach(appendTask);
 
-tasks.forEach( (task) => {
-    const newLi = document.createElement('li');
-    newLi.textContent = task;
-    document.getElementById('todo-list').appendChild(newLi);
-})
+tasks.forEach(task => {
+  const newLi = document.createElement("li");
+  newLi.textContent = task;
+  document.getElementById("todo-list").appendChild(newLi);
+});
 
 // - Create a css class (e.g. `.done`) that, when applied on an element, adds the
 //   `text-decoration: line-through` property
@@ -30,75 +25,65 @@ tasks.forEach( (task) => {
 //   (i.e. add/remove) the `.done` css class (hint: take a look at the
 //   documentation for `.classList.toggle()`)
 
-var linkLi = document.querySelectorAll('li');
+var listItems = document.querySelectorAll("li");
 
-function loopEach(eachLi){
-	eachLi.addEventListener('click', (event) => {
-		event.target.classList.toggle('lineThrough')
-})
-};
+function addClickHandler(eachLi) {
+  eachLi.addEventListener("click", event => {
+    event.target.classList.toggle("lineThrough");
+  });
+}
 
-linkLi.forEach(loopEach);
-
+listItems.forEach(addClickHandler);
 
 // ### Adding more To-Do items
 
 // - Add a `<input>` HTML element and a `<button>Add Todo</button>` HTML element.
 
-const newInput = document.createElement('input');
-document.body.appendChild(newInput).insertAdjacentHTML('afterend', '<button>Add ToDo</button>');
-
+const newInput = document.createElement("input");
+document.body
+  .appendChild(newInput)
+  .insertAdjacentHTML("afterend", "<button>Add ToDo</button>");
 
 // - Add a 'click' event listener to the button that adds the value of the
 //   `<input>` element as a new todo in the `ul#todo-list` (follow the 3 steps for
-//   creating and appending a new DOM element - 
+//   creating and appending a new DOM element -
 
+function addToDoItem() {
+	var addNewLi = document.createElement("li");
+    addClickHandler(addNewLi);
+    addNewLi.textContent = document.querySelector("input").value;
+    document.getElementById("todo-list").appendChild(addNewLi);
+    addNewBtn(addNewLi);
+    document.querySelector("input").value = "";
+}
 
-document.querySelector('button').addEventListener('click', () => {
-// (i) `createElement`,
-	var addNewLi = document.createElement('li');
-// // (ii) change any attributes if necessary,
-	loopEach(addNewLi);
-	addNewLi.textContent = document.querySelector('input').value;
-// // (iii) use `appendChild` to append it to the appropriate HTML element)
-	document.getElementById('todo-list').appendChild(addNewLi);
-	addNewBtn(addNewLi);
-	document.querySelector('input').value = '';
-	});
+document.querySelector("button").addEventListener("click", () => {
+	addToDoItem();
+});
 
-// - Add css styling and make this a nice todo list that you can show off!
-
-// - Bonus: Allow user to create a todo when they press the <kbd>Enter</kbd> key
-
-
-document.addEventListener('keypress', () => {
-	if(event.keyCode == 13 && document.querySelector('input').value != '') {
-		var addNewLi = document.createElement('li');
-		loopEach(addNewLi);
-		addNewLi.textContent = document.querySelector('input').value;
-		document.getElementById('todo-list').appendChild(addNewLi);
-		addNewBtn(addNewLi);
-		document.querySelector('input').value = '';
-		}
-	});
+document.addEventListener("keypress", () => {
+  if (event.keyCode == 13 && document.querySelector("input").value != "") {
+    addToDoItem()
+  }
+});
 
 // Add cancel buttons
 
-function addNewBtn(eachLi){
-	const newBtn = document.createElement('button');
-	newBtn.textContent = 'x';
-	newBtn.classList.add('cancelBtn');
-	addCancelf(newBtn);
-	eachLi.appendChild(newBtn)
-};
+function addNewBtn(eachLi) {
+  const newBtn = document.createElement("button");
+  newBtn.textContent = "x";
+  newBtn.classList.add("cancelBtn");
+  addCancelf(newBtn);
+  eachLi.appendChild(newBtn);
+}
 
-linkLi.forEach(addNewBtn);
+listItems.forEach(addNewBtn);
 
-var cancelBtns = document.querySelectorAll('.cancelBtn');
+var cancelBtns = document.querySelectorAll(".cancelBtn");
 
-function addCancelf(eachBtn){
-	eachBtn.addEventListener('click', (event) => {
-		var liToRemove = event.target.parentNode;
-		liToRemove.remove();
-})
-};
+function addCancelf(eachBtn) {
+  eachBtn.addEventListener("click", event => {
+    var liToRemove = event.target.parentNode;
+    liToRemove.remove();
+  });
+}
